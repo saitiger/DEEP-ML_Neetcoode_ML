c1461acc647e848c65648d118ebeb9777a5b7db8
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-10-25 23:08:51 -0700

    Create Implement Recall Metric in Binary Classification.py

---
+++ Deep-ML/Implement Recall Metric in Binary Classification.py
@@ -0,0 +1,20 @@
+import numpy as np
+def recall(y_true, y_pred):
+  """
+  Mnemonics to Remember 
+  Precision and Recall
+  
+  Precision **Predicted** from **Pre**cision positive: TP/(TP+FP)
+  Recall is **Real** from **Re**call : TP/(TP+FN)
+
+  Sensitivity : TP/(TP+FN)
+  Specificity : TN/(TN+FP)
+
+  Mnemonics to Remember 
+  
+  SNIP (Sensitivity Is Positive)
+  SPIN (Specificity Is Negative)
+  """
+    true_positives = np.sum((y_true == 1) & (y_pred == 1))
+    false_negatives = np.sum((y_true == 1) & (y_pred == 0))
+    return true_positives / (true_positives + false_negatives) if (true_positives + false_negatives) > 0 else 0.0
