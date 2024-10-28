8ed8b1b657c2be4a19cc29331d47496bfdbeb5bf
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-10-27 21:00:22 -0700

    Update Implement Recall Metric in Binary Classification.py

--- Deep-ML/Implement Recall Metric in Binary Classification.py
+++ Deep-ML/Implement Recall Metric in Binary Classification.py
@@ -17,4 +17,4 @@ def recall(y_true, y_pred):
   """
     true_positives = np.sum((y_true == 1) & (y_pred == 1))
     false_negatives = np.sum((y_true == 1) & (y_pred == 0))
-    return true_positives / (true_positives + false_negatives) if (true_positives + false_negatives) > 0 else 0.0
+    return round(true_positives / (true_positives + false_negatives) if (true_positives + false_negatives) > 0 else 0.0,3)
